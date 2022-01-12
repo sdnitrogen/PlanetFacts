f29b3fd0cb4870253b1820117865ecc76ad034a3
sdnitrogen <skd.bwn96@gmail.com>
2022-01-11 22:59:46 -0700

    Planet detail page layout in progress

---
+++ app/src/main/java/red/nitrogen/planetfacts/PlanetDetailActivity.kt
@@ -0,0 +1,11 @@
+package red.nitrogen.planetfacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlanetDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_planet_detail)
+    }
+}
